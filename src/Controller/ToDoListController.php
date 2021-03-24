e4031ca9496950e85983851a38c53c47c99737ff
vasilhsscour <vasilhsscour@gmail.com>
2021-03-24 19:50:38 +0200

    Create a ToDoList Controler

---
+++ src/Controller/ToDoListController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToDoListController extends AbstractController
+{
+    /**
+     * @Route("/to/do/list", name="to_do_list")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ToDoListController.php',
+        ]);
+    }
+}
