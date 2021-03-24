1e03a7d1259e8032f44761c706498d1dfc31af72
vasilhsscour <vasilhsscour@gmail.com>
2021-03-24 20:15:36 +0200

    Display the templae of the ToDoList

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -9,13 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class ToDoListController extends AbstractController
 {
     /**
-     * @Route("/to/do/list", name="to_do_list")
+     * @Route("/", name="to_do_list")
      */
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ToDoListController.php',
-        ]);
+        return $this->render('index.html.twig');
     }
 }
