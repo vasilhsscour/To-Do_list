f859b89157e32cb8991e79b2c0f8f43b73ebe38e
vasilhsscour <vasilhsscour@gmail.com>
2021-03-25 19:04:46 +0200

    Add method for Create, Update and Delete a Task

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -15,4 +15,28 @@ class ToDoListController extends AbstractController
     {
         return $this->render('index.html.twig');
     }
+
+    /**
+     * @Route("/create", name="createTask", methods={"POST"})
+     */
+    public function createTask(): Response
+    {
+        exit("to do: create a new task!");
+    }
+
+    /**
+     * @Route("/switchStatus/{id}", name="switchStatus")
+     */
+    public function switchStatus($id): Response
+    {
+        exit("to do: switch status of the tast : {$id}");
+    }
+
+    /**
+     * @Route("/delete/{id}", name="deleteTask")
+     */
+    public function deleteTask($id): Response
+    {
+        exit("to do: delete a task with id of : {$id}");
+    }
 }
